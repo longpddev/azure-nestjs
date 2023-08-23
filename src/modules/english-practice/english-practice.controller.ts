25a62f1c48bbbf4c522baa880d37ce611427ee95
longpd <longpddev@gmail.com>
2023-08-23 20:55:13 +0700

    update prompt

--- src/modules/english-practice/english-practice.controller.ts
+++ src/modules/english-practice/english-practice.controller.ts
@@ -9,6 +9,26 @@ class EvaluateBodyDto {
 }
 @Controller('english-practice')
 export class EnglishPracticeController {
+  private topics = [
+    'Solve problems in life',
+    'plan something',
+    'target hobby',
+    'interest',
+    'game',
+    'travel',
+    'Animals',
+    'Education',
+    'Ethics',
+    'Health',
+    'Sports',
+    'Technology',
+    'random',
+  ];
+
+  pickTopic() {
+    return this.topics.at(Math.round(Math.random() * (this.topics.length - 1)));
+  }
+
   constructor(private readonly aiModel: AiModelService) {}
   @Get('exercise')
   async exercise() {
@@ -16,7 +36,7 @@ export class EnglishPracticeController {
     prompt =
       'As an English teacher, you design a writing exercise for your beginner students. Within this exercise, you construct a random scenario or context. Subsequently, your students are required to write a response based on the given scenario. This type of question is commonly encountered in the TOEIC test, a proficiency exam for English learners.';
 
-    prompt = `You create an open-ended question on any topic.
+    prompt = `You create an open-ended question by follow ${this.pickTopic()} topic.
     For example:
     - Tell me about your day.
     - You want to ask your friend to go to a new city on Saturday. how will you invite, what city has,...
@@ -25,7 +45,7 @@ export class EnglishPracticeController {
         2. It suddenly rains heavily. 
         3. Your tile is flipped.
         4. You call your relatives. 
-        5. Friends are happy to see them coming. 
+        5. You are happy to see them coming. 
         Let's string together the situations above and tell them like a story.
     ....et cetera
     
